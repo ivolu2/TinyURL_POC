a44017b948cefcf106c6f465c0a2dc1dce7a7f2f
Ivan Lu <ivanlu2@yahoo.com>
2023-11-14 16:34:53 -0600

    first commit

---
+++ TinyURL_POC/TinyURLService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+namespace TinyURL_POC
+{
+	public class TinyURLService
+	{
+
+		public List<string> tokens = new List<string>();
+
+		public Dictionary<string, (string longUrl, int visitedCount)> tinyURLs = new Dictionary<string, (string, int)>();
+
+
+        public string createTinyURL(string longURL)
+		{
+
+			//Generating unique tokens based on "characters"
+            const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+
+            string uniqueKey = new string(Enumerable.Repeat(characters, 6)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+			//If token is NOT unique, create a new one (Ensuring that token is unique)
+			while (tokens.Contains(uniqueKey))
+			{
+				uniqueKey = new string(Enumerable.Repeat(characters, 6)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            }
+
+			//Add unique key to tokens list
+			tokens.Add(uniqueKey);
+
+			//Add new tinyURL to list
+            string tinyUrl = "https://www.tinyurl/" + uniqueKey;
+			tinyURLs.Add(tinyUrl,(longURL,0));
+		
+			return tinyUrl;
+		}
+
+		public void deleteTinyURL(string tinyURL)
+		{
+			//Search for tinyURL (key) in dictionary
+			if (tinyURLs.ContainsKey(tinyURL)) 
+			{
+				//Key Found
+				//Grabbing token from tinyURL
+				string token = tinyURL.Substring(20);
+
+				//Checking if token exists in list, delete from list if so
+                if (tokens.Contains(token))
+                {
+                    tokens.Remove(token);
+                }
+
+				//Removing tinyURL from dictionary
+                tinyURLs.Remove(tinyURL);
+			}
+
+		}
+
+		public string getLongURL(string tinyURL)
+		{
+			//Search for tinyURL (key) in dictionary
+			if(tinyURLs.TryGetValue(tinyURL, out var value)) 
+			{
+				//Key found, return longURL
+				return value.longUrl;
+			}
+			else 
+			{
+                return "err";
+            }
+			
+		}
+
+		public bool tinyURLExist(string tinyURL)
+		{
+            //Search for tinyURL (key) in dictionary
+            if (tinyURLs.ContainsKey(tinyURL)) 
+            {
+				//Key is found, return true
+				return true;
+            }
+			else 
+			{
+                return false;
+            }
+           
+		}
+
+		public int getStats(string tinyURL)
+		{
+            //Search for tinyURL (key) in dictionary
+            if (tinyURLs.ContainsKey(tinyURL)) 
+            {
+				//Key is found so return stats
+				var urlData = tinyURLs[tinyURL];
+                return urlData.visitedCount;
+            }
+            else 
+            {
+				return -1;
+            }
+
+		}
+
+		public int visit(string tinyURL)
+		{
+            //Search for tinyURL (key) in dictionary
+            if (tinyURLs.ContainsKey(tinyURL)) 
+            {
+				//Key is found so update visitedCount
+				var urlData = tinyURLs[tinyURL];
+				urlData.visitedCount = urlData.visitedCount+1;
+
+				
+				tinyURLs[tinyURL] = urlData;
+
+                return urlData.visitedCount;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+
+	}
+}
+
---
+++ tinyURLTests/tinyURLTests.cs
@@ -0,0 +1,103 @@
+﻿using TinyURL_POC;
+
+namespace tinyURLTests;
+
+[TestClass]
+public class tinyURLTests
+{
+    [TestMethod]
+    public void TestCreateTinyURL()
+    {
+        //Arrange
+        TinyURLService tinyURLService = new();
+
+        //Act
+        string longURL = "https://www.google.com";
+        string tinyURL = tinyURLService.createTinyURL(longURL);
+        string token = tinyURL.Substring(20);
+
+        //Assert
+        Assert.IsNotNull(tinyURL); //check if tinyURL is null
+        Assert.IsTrue(Uri.IsWellFormedUriString(tinyURL, UriKind.Absolute)); //check if tinyURL is of valid format for Uri
+        Assert.IsTrue(tinyURLService.tinyURLs.ContainsKey(tinyURL)); //check if tinyURL mapping was added
+        Assert.IsTrue(tinyURLService.tinyURLs[tinyURL].longUrl == longURL); //check if tinyURL to longURL mapping is correct
+        Assert.IsNotNull(token); //check if token is null
+        Assert.IsTrue(tinyURLService.tokens.Contains(token)); //check if token was added 
+
+    }
+
+    [TestMethod]
+    public void TestDeleteTinyURL()
+    {
+        //Arrange
+        TinyURLService tinyURLService = new();
+
+        //Act
+        string longURL = "https://www.google.com";
+        string tinyURL = tinyURLService.createTinyURL(longURL);
+        string token = tinyURL.Substring(20);
+        tinyURLService.deleteTinyURL(tinyURL);
+
+        //Assert
+        Assert.IsFalse(tinyURLService.tinyURLs.ContainsKey(tinyURL)); //Check if tinyURL mapping was deleted
+        Assert.IsFalse(tinyURLService.tokens.Contains(token)); //Check if token was deleted
+
+    }
+
+    [TestMethod]
+    public void TestGetLongURL()
+    {
+        //Arrange
+        TinyURLService tinyURLService = new();
+
+        //Act
+        string longURL = "https://www.google.com";
+        string tinyURL = tinyURLService.createTinyURL(longURL);
+        string getLongURL = tinyURLService.getLongURL(tinyURL);
+
+        //Assert
+        Assert.IsNotNull(tinyURL); //check if tinyURL is null
+        Assert.IsTrue(Uri.IsWellFormedUriString(tinyURL, UriKind.Absolute)); //check if tinyURL is of valid format for Uri
+        Assert.IsTrue(Uri.IsWellFormedUriString(longURL, UriKind.Absolute)); //check if longURL is of valid format for Uri
+        Assert.IsNotNull(getLongURL); //check if getLongURL is null
+        Assert.IsTrue(Uri.IsWellFormedUriString(getLongURL, UriKind.Absolute)); //check if getLongURL is of valid format for Uri
+        Assert.IsTrue(longURL == getLongURL); //check if longURL == getLongURL
+
+    }
+
+    [TestMethod]
+    public void TestVisitURL()
+    {
+        //Arrange
+        TinyURLService tinyURLService = new();
+
+        //Act
+        string longURL = "https://www.google.com";
+        string tinyURL = tinyURLService.createTinyURL(longURL);
+        int count = tinyURLService.visit(tinyURL);
+
+        //Assert
+        Assert.IsNotNull(tinyURL); //Check if tinyURL is null
+        Assert.IsTrue(count == 1); //Check if visit value was updated
+
+    }
+
+    [TestMethod]
+    public void TestGetStats()
+    {
+        //Arrange
+        TinyURLService tinyURLService = new();
+
+        //Act
+        string longURL = "https://www.google.com";
+        string tinyURL = tinyURLService.createTinyURL(longURL);
+        int count = tinyURLService.visit(tinyURL);
+        count = tinyURLService.visit(tinyURL);
+        count = tinyURLService.visit(tinyURL); 
+
+        //Assert
+        Assert.IsNotNull(tinyURL); //Check if tinyURL is null
+        Assert.IsTrue(count == 3); //Check if Visit Count is equal to 3 (3 visit calls)
+    }
+    
+}
